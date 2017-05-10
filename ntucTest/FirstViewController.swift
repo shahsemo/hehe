44327523832eaedc5c3a2c28c98ee1d6d9a0b684
Chia Li Yun <153624f@sit.nyp.edu.sg>
2017-05-10 22:49:02 +0800

    Update name

--- ntucTest/FirstViewController.swift
+++ ntucTest/FirstViewController.swift
@@ -8,9 +8,8 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class FirstViewController: UIViewController{
     
-    @IBOutlet weak var imagePicked: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,49 +21,6 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func openCameraButton(sender: AnyObject){
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
-            imagePicker.allowsEditing = false
-            self.present(imagePicker, animated: true, completion: nil)
-            
-        }
-    }
     
-    @IBAction func openPhotoLibraryButton(sender: AnyObject){
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
-            imagePicker.allowsEditing = true
-            self.present(imagePicker, animated: true, completion: nil)
-        }
-    }
-    
-    @IBAction func saveImageBtn(sender: AnyObject) {
-        let imageData = UIImageJPEGRepresentation(imagePicked.image!, 0.6)
-        let compressedJPGImage = UIImage(data: imageData!)
-        UIImageWriteToSavedPhotosAlbum(compressedJPGImage!, nil, nil, nil)
-        
-        let alert = UIAlertView(title: "Wow", message: "Your image has been saved to Photo Library", delegate: nil, cancelButtonTitle: "Okay")
-        
-        alert.show()
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        for (key, value) in info {
-            if key == UIImagePickerControllerOriginalImage {
-                print(value)
-                //                if value as? UIImage != nil {
-                let image = value as? UIImage
-                imagePicked.image = image
-                self.dismiss(animated: true, completion: nil)
-                //                }
-                
-            }
-        }
-    }
 }
 
