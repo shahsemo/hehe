c91c51a16bce68a2fe0aadec4b7becdb60aa93e8
Tan QingYu <qingyu@Bibimbap.local>
2017-07-04 20:37:10 +0800

    Remove HomeTableViewController and change to HomeViewController for changes upcoming and added a few more categories

--- ShareATextbook/HomeContentTableViewCell.swift
+++ ShareATextbook/HomeContentTableViewCell.swift
@@ -9,18 +9,20 @@
 import UIKit
 
 class HomeContentTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func awakeFromNib() {
+        
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
--- ShareATextbook/HomeViewController.swift
+++ ShareATextbook/HomeViewController.swift
@@ -2,20 +2,54 @@
 //  HomeViewController.swift
 //  ShareATextbook
 //
-//  Created by Tan QingYu on 27/6/17.
+//  Created by Tan QingYu on 4/7/17.
 //  Copyright © 2017 Chia Li Yun. All rights reserved.
 //
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // HAVEN'T FIX THE CATEGORIES IMAGE
+    
+    var categories = [["name" : "TEXTBOOK","img": "cells"], ["name" : "TYS","img": "cells"], ["name" : "PRIMARY 1","img": "cells"], ["name" : "PRIMARY 2","img": "cells"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-  
-
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return categories.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! HomeContentTableViewCell
+        
+        let bookCat = categories[indexPath.row]
+        
+        let s = bookCat["img"]!
+        cell.backgroundImage.image = UIImage(named: s)
+        cell.nameLabel.text = bookCat["name"]
+        
+        
+        
+        return cell
+    }
+    
+    
 }
